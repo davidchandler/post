02ba51125355b58827042ce8060a731188d75be2
David Chandler <dchandler43@hotmail.com>
2013-09-02 17:38:13 -0700

    CSS Formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
 
+  def display_date(date)
+      if logged_in?
+        date = date.in_time_zone(current_user.time_zone)
+      end
+      date.strftime('%B %d, %Y at %I:%M %p %Z')
+    end
+
 end
